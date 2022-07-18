92b81b69929b473f4ec1515d25926ec1c063b240
relt <haxrelt@gmail.com>
2022-07-19 00:56:38 +0300

    Реализовал рывок(dash) в сторону курсора

--- Assets/Scripts/Input/Actor.cs
+++ Assets/Scripts/Input/Actor.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class Actor : MonoBehaviour
 {
     [SerializeField] private float speed;
+
+    [SerializeField] private float dashCooldown = 20;
+    [SerializeField] protected float dashSpeed = 15f;
+    [SerializeField] protected int dashDuration = 35;
+
+    protected float dashDelay = 0;
+    protected float dashActive = 0;
+    protected bool dashing = false;
+    protected Vector2 dashDirection;
+
     protected Rigidbody2D rigidbody;
 
     private void Awake()
@@ -20,5 +30,23 @@ public class Actor : MonoBehaviour
     protected void Fire()
     {
 
+    }
+
+    protected void Dash(Vector2 direction)
+    {
+        if (dashDelay > 0) return;
+        dashing = true;
+
+        dashDirection = direction;
+
+        rigidbody.velocity = new Vector2(direction.x, direction.y);
+        rigidbody.AddForce(direction * dashSpeed, ForceMode2D.Force);
+        dashDelay = dashCooldown;
+    }
+
+    protected void StopDash()
+    {
+        dashDirection = Vector2.zero;
+        dashing = false;
     }
 }
--- Assets/Scripts/Input/PlayerMovement.cs
+++ Assets/Scripts/Input/PlayerMovement.cs
@@ -1,18 +1,47 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerMovement : Actor
 {
     [SerializeField] private GameObject spawnTest;
+    [SerializeField] private TMP_Text dashCooldownText;
+
     void Update()
     {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //if (dashing) return;
+
+        
+
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
         Move(new Vector2(x, y));
 
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            SpawnClick();
+            Dash(((Vector3)mousePosition - transform.position).normalized);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (dashDelay > 0)
+        {
+            dashCooldownText.text = dashDelay.ToString() + " s.";
+            dashDelay--;
+        }
+
+        if (dashing)
+        {
+            rigidbody.velocity = (dashDirection * dashSpeed);
+            dashActive++;
+            if(dashActive > dashDuration)
+            {
+                StopDash();
+                dashActive = 0;
+            }
         }
     }
 
